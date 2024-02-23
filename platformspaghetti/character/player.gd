ceaf55f9c55a491674e6bc595931b1ee80a063e7
Stefano Rapuano <srapuano@e3r3p6.42roma.it>
2024-02-23 15:54:11 +0100

    prima_bozza

---
+++ platformspaghetti/character/player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+
+@export var speed : float = 200.0
+@export var hump_velocity : float = -400.0
+
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var animation_locked : bool = false
+var direction : Vector2 = Vector2.ZERO
+func _physics_process(delta):
+	# Add the gravity. 
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = hump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction:
+		velocity.x = direction.x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
+	
+func update_animation():
+	if not animation_locked:
+		if direction.x != 0:
+			animated_sprite.play("walk")
+		else:
+			animated_sprite.play("jump")
